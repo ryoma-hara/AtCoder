f31ace191bc2535277c172300bd4d04726067962
ryoma-hara <toooooom@icloud.com>
2020-05-04 23:42:38 +0900

    AC

--- ABC086/C.rb
+++ ABC086/C.rb
@@ -2,10 +2,11 @@
 n = gets.to_i
 
 n.times do 
-  t,x,y = gets.split(' ').map(&:to_i)
-  if t % 2 == (x+y) % 2 && (x+y) <= t
-    puts 'YES'
-  else
-    puts 'NO'
+  t,x,y = gets.chomp.split(' ').map(&:to_i)
+  unless t % 2 == (x+y) % 2 && (x+y) <= t
+    puts 'No'
+    exit
   end
 end
+
+puts 'Yes'
