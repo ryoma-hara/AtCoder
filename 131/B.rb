18fac9caf6c037baf4b78b3c97adcc29f5089ae2
ryoma-hara <toooooom@icloud.com>
2020-06-12 14:59:59 +0900

    AC(無駄なコードを削除)

--- 131/B.rb
+++ 131/B.rb
@@ -1,21 +1,13 @@
-# i の味は l + i - 1 
 n,l = gets.split.map(&:to_i)
 ans = []
-if l >= 0 
-  (2..n).each do |i|
+(1..n).each do |i|
     ans << l + i - 1
-  end
-else 
-  (1..n).each do |i|
-    ans << l + i - 1
-  end
 end
-sum = ans.inject(:+)
 
 if ans.include?(0)
-  p sum
-elsif sum > 0
-  p sum
+  p ans.inject(:+)
+elsif ans.inject(:+) > 0
+  p ans.inject(:+) - ans.min
 else
-  p sum - ans.max
+  p ans.inject(:+) - ans.max
 end
